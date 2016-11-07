8e6914dc3758728574c37d6f698122c67792ccba
MirekSz <m.szajowski@gmail.com>
2016-11-07 18:46:39 +0100

    sd

---
+++ spring-boot-crud/src/test/java/hello/BaseTest.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@Rollback
+@Transactional
+@ActiveProfiles("test")
+@ContextConfiguration
+public abstract class BaseTest {
+
+}
