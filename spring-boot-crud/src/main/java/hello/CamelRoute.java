30237c485c752d31a3e44bd78d45a11d730724c5
MirekSz <m.szajowski@gmail.com>
2016-11-10 15:22:14 +0100

    hand

--- spring-boot-crud/src/main/java/hello/CamelRoute.java
+++ spring-boot-crud/src/main/java/hello/CamelRoute.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,39 @@ public class CamelRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		// onException(NullPointerException.class).handled(true).to("file:C:\\temp\\old\\failed");
 		from("jetty:http://localhost:5345").removeHeaders("*", "name").to("log:bar?showHeaders=true");
+
+		from("file:C:\\temp\\old\\2014-01-09?moveFailed=C:\\temp\\old\\failed_move").process(new Processor() {
+
+			@Override
+			public void process(Exchange arg0) throws Exception {
+				// TODO Auto-generated method stub
+
+			}
+		}).id("a").process(new Processor() {
+
+			@Override
+			public void process(Exchange arg0) throws Exception {
+				// TODO Auto-generated method stub
+
+			}
+		}).id("b").process(new Processor() {
+
+			@Override
+			public void process(Exchange arg0) throws Exception {
+				// TODO Auto-generated method stub
+
+			}
+		}).id("c").process(new Processor() {
+
+			@Override
+			public void process(Exchange arg0) throws Exception {
+				if (2 > 1) {
+					throw new NullPointerException();
+				}
+
+			}
+		}).id("d");
 	}
 }
