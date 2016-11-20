206e4abdb24461484e3ad7c5b1305d4100ed6743
MirekSz <miro1994>
2016-11-20 13:21:53 +0100

    fix

--- spring-boot-crud/src/main/java/hello/sec/WebSecurityConfig.java
+++ spring-boot-crud/src/main/java/hello/sec/WebSecurityConfig.java
@@ -26,74 +26,80 @@ import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
 	@EventListener
 	public void handle(AuthorizationFailureEvent envent) {
 		System.out.println(envent);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// http.addFilterBefore(filter,
-		// UsernamePasswordAuthenticationFilter.class);
-		http.authorizeRequests().antMatchers("/assets/**", "/login**", "/logout**", "/services/**").permitAll()
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").and().logout().logoutUrl("/logout")
-				.and().rememberMe().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-				.and().exceptionHandling().and().httpBasic()
-				.authenticationEntryPoint(new AjaxAwareAuthenticationEntryPoint("/login"));
-		http.headers().frameOptions().disable();
+	@Configuration
+	@Order(2)
+	public static class FormConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// http.addFilterBefore(filter,
+			// UsernamePasswordAuthenticationFilter.class);
+			http.antMatcher("/**").authorizeRequests()
+					.antMatchers("/assets/**", "/login**", "/logout**", "/services/**").permitAll().anyRequest()
+					.authenticated().and().formLogin().loginPage("/login").and().logout().logoutUrl("/logout").and()
+					.rememberMe().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+					.exceptionHandling().and().httpBasic()
+					.authenticationEntryPoint(new AjaxAwareAuthenticationEntryPoint("/login"));
+			http.headers().frameOptions().disable();
 
-		// AccessDeniedHandlerImpl deniedhandler = new
-		// AccessDeniedHandlerImpl();
-		// deniedhandler.setErrorPage("/accessdenied");
-		// http.authorizeRequests()
-		// .antMatchers("/welcome", "/api/ping", "/api/cookie", "/signup",
-		// "loginAjax", "/about", "/register", "/currentUser", "/", "/welcome")
-		// .permitAll().antMatchers("/api/admin/**").hasRole("ADMIN")
-		// .antMatchers("/api/appContext").hasRole("ADMIN")
-		// .antMatchers("/role/**").hasRole("ADMIN")
-		// .antMatchers("/role/*").hasRole("ADMIN")
-		// /* .antMatchers("/products/**").hasAnyRole("USER")
-		// .antMatchers("/products/*").hasAnyRole("USER")*/
-		// .antMatchers("/api/user/**").hasRole("USER")
-		// .antMatchers("/currentUser").hasRole("USER")
-		// .antMatchers("/api/business**").access("hasRole('ROLE_ADMIN') and
-		// hasRole('ROLE_BUSINESS')").anyRequest().authenticated();
-		// http.headers().disable();
-		// http.csrf().disable()
-		// .formLogin().loginPage("/login").successHandler(authSuccessHandlerImpl).failureHandler(authFailureHandlerImpl).failureUrl("/login?error=true").defaultSuccessUrl("/").permitAll()
-		// .and()
-		// .logout().logoutUrl("/logout").logoutSuccessUrl("/welcome").invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll()
-		// .and().exceptionHandling().accessDeniedHandler(deniedhandler);
-		// //@formatter:on
-	}
+			// AccessDeniedHandlerImpl deniedhandler = new
+			// AccessDeniedHandlerImpl();
+			// deniedhandler.setErrorPage("/accessdenied");
+			// http.authorizeRequests()
+			// .antMatchers("/welcome", "/api/ping", "/api/cookie", "/signup",
+			// "loginAjax", "/about", "/register", "/currentUser", "/",
+			// "/welcome")
+			// .permitAll().antMatchers("/api/admin/**").hasRole("ADMIN")
+			// .antMatchers("/api/appContext").hasRole("ADMIN")
+			// .antMatchers("/role/**").hasRole("ADMIN")
+			// .antMatchers("/role/*").hasRole("ADMIN")
+			// /* .antMatchers("/products/**").hasAnyRole("USER")
+			// .antMatchers("/products/*").hasAnyRole("USER")*/
+			// .antMatchers("/api/user/**").hasRole("USER")
+			// .antMatchers("/currentUser").hasRole("USER")
+			// .antMatchers("/api/business**").access("hasRole('ROLE_ADMIN') and
+			// hasRole('ROLE_BUSINESS')").anyRequest().authenticated();
+			// http.headers().disable();
+			// http.csrf().disable()
+			// .formLogin().loginPage("/login").successHandler(authSuccessHandlerImpl).failureHandler(authFailureHandlerImpl).failureUrl("/login?error=true").defaultSuccessUrl("/").permitAll()
+			// .and()
+			// .logout().logoutUrl("/logout").logoutSuccessUrl("/welcome").invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll()
+			// .and().exceptionHandling().accessDeniedHandler(deniedhandler);
+			// //@formatter:on
+		}
 
-	private CsrfTokenRepository csrfTokenRepository() {
-		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-		// repository.setHeaderName("sec-token");
-		return repository;
-	}
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			// repository.setHeaderName("sec-token");
+			return repository;
+		}
 
-	// @Autowired
-	// public void configureGlobal(UserDetailsService userDetailsService,
-	// AuthenticationManagerBuilder auth,
-	// PasswordEncoder passwordEncoder) throws Exception {
-	// auth.userDetailsService(userDetailsService);
-	// }
+		// @Autowired
+		// public void configureGlobal(UserDetailsService userDetailsService,
+		// AuthenticationManagerBuilder auth,
+		// PasswordEncoder passwordEncoder) throws Exception {
+		// auth.userDetailsService(userDetailsService);
+		// }
 
-	@Autowired
-	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
-		// @formatter:off
-		auth.userDetailsService(new UserDetailsService() {
+		@Autowired
+		public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+			// @formatter:off
+			auth.userDetailsService(new UserDetailsService() {
 
-			@Override
-			public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-				return new SystemUser();
-			}
-		});// .passwordEncoder(new BCryptPasswordEncoder());
-			// auth.inMemoryAuthentication().withUser("mirek").password("mirek").roles("USER");
+				@Override
+				public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+					return new SystemUser();
+				}
+			});// .passwordEncoder(new BCryptPasswordEncoder());
+				// auth.inMemoryAuthentication().withUser("mirek").password("mirek").roles("USER");
 
+		}
 	}
 
 	@Configuration
@@ -107,7 +113,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			// .hasRole("API")
-			http.authorizeRequests().antMatchers("/api/**").authenticated().and().httpBasic().and().csrf().disable();
+			http.antMatcher("/api/**").authorizeRequests().antMatchers("/**").authenticated().and().httpBasic().and()
+					.csrf().disable();
 		}
 	}
 
