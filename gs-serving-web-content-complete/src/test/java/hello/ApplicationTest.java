3a4be9564cc05897ce6a4f981d2aee45e71c6b12
MirekSz <miro1994>
2017-11-11 14:46:47 +0100

    co

--- gs-serving-web-content-complete/src/test/java/hello/ApplicationTest.java
+++ gs-serving-web-content-complete/src/test/java/hello/ApplicationTest.java
@@ -31,11 +31,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Rollback(true)
+@Transactional
+@Rollback
 public class ApplicationTest {
 
 	@Autowired
